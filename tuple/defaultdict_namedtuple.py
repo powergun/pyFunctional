ddc48b9938c4bfc4a4450f099d4481de1712591c
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-06-18 07:19:28 +1000

    more defaultdict + namedtuple

--- tuple/defaultdict_namedtuple.py
+++ tuple/defaultdict_namedtuple.py
@@ -9,14 +9,25 @@ class CallCount(NamedTuple):
     @staticmethod
     def empty():
         return CallCount(0, 0)
-
+    
+    def __iadd__(self, o):
+        return CallCount(self.count + o[0], self.cost + o[1])
 
 def main():
     # use the second form of defaultdict ctor that takes a "default factory"
     # function; this function produces a value of type T
     rec = defaultdict(CallCount.empty)
     print(rec['asd'])
-    rec['asd'] += (1, 1)
+    
+    # NOTE:
+    # with overloading __iadd__() method, this will produce a plain tuple
+    # rec['asd'] += (1, 1)
+
+    rec['asd'] = CallCount(rec['asd'].count + 1, rec['asd'].cost + 10)
+    print(rec['asd'])
+    
+    rec['bsd'] += (9, 9)
+    print(rec['bsd'])
 
 
 if __name__ == '__main__':
